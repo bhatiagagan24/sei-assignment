a2005cdd6abbfab1abcf1f90b951ed3466003b07
Gagandeep Bhatia <gagandeepbhatia@Gagandeeps-MacBook-Air.local>
2024-08-19 11:49:11 +0530

    setting up gitignore

--- openai-utils.py
+++ openai-utils.py
@@ -8,11 +8,21 @@ class OpenApiClient:
     def __init_client(self, api_key):
         client = client = OpenAI(api_key=api_key)
         return client
-    def get_prompt(self):
+    def load_stripe_checks(self):
+        pass
+    def __get_prompt(self):
+        messages=[
+            {
+                "role": "system", "content": f'''
+             Assume you are a compliance officer and this is the list of check you need to perform on a bank's website 
+             '''
+            }
+        ],
         return '''
             Assume you are a compliance officer and given this list of compliance checks, please provide a list of changes required 
             in the website text given at the bottom to make it compliant 
             '''
+    def get_response(self):pass
 
 client = OpenAI(
     # Defaults to os.environ.get("OPENAI_API_KEY")
